421f155a07c4b65f4defad15a30a1b44cd1eb6ea
Geoffrey Tipton <tipton@mysecretroom.com>
2019-04-08 16:19:01 +0800

    got all IDs on all pages.

--- main.go
+++ main.go
@@ -1,108 +1,92 @@
 package main
 
 import (
+    "fmt"
     "github.com/PuerkitoBio/goquery"
-    "io"
     "log"
     "net/http"
     "os"
+    "strconv"
     "sync"
     "time"
 )
 
 var waitGroup sync.WaitGroup //to ensue the download is complete before next file
-var urls = getUrls()
 
 func main() {
-    for _ , url := range urls {
-        getPage(url)
-        log.Print(url)
-    }
+    getPages("All")
 }
 
-func getPage(_url string) {
+// This may be modified with search criteria
+func getUrls(key string) []string {
+    return []string{"https://www.zoopla.co.uk/for-sale/houses/manchester/?q=Manchester&radius=0&results_sort=newest_listings&search_source=refine"}
+}
+
+func getPages( filter string) {
+    getZoopla(filter)
+
+    return
+}
+
+func getZoopla(filter string){
     defer func() {
         r := recover()
         if r != nil {
             log.Println("Request Disconnection ignored:", r)
         }
     }()
+    pageNo , totalPages := 1 , 2
+    var fullIdList []int64
+    for {
+        var url = fmt.Sprintf("https://www.zoopla.co.uk/for-sale/houses/manchester/?q=Manchester&radius=0&results_sort=newest_listings&search_source=refine&pn=%d",pageNo)
+        client := &http.Client{Timeout:30 * time.Second}
+        request, err := http.NewRequest("GET", url, nil )
+        if err != nil{log.Println("Make New request", err)}
+        request.Header.Set("User-Agent", "Stealing your Data")
 
-    client := &http.Client{Timeout:30 * time.Second}
-    request, err := http.NewRequest("GET", _url,nil )
-    if err != nil {
-        log.Println("Make New request")
-        log.Fatal(err) }
-    request.Header.Set("User-Agent", "Stealing your Data")
-
-    // Make HTTP GET request
-    response, err := client.Do(request )
-    if err != nil {
-        log.Println("Make HTTP request")
+        // Make HTTP GET request
+        response, err := client.Do(request )
+        if err != nil{ log.Println("Make HTTP request") }
+        defer response.Body.Close()
+        document, err := goquery.NewDocumentFromReader(response.Body)
+        if err != nil{log.Println("Error loading HTTP response body. ")}
+        if pageNo == 1 { // Get a page count on the first load
+            totalPages = getTotalPages(document)
+        }
+        fullIdList = append(fullIdList, getIdList(document )... )
+        log.Print(fullIdList)
+        if pageNo == totalPages {
+            break
+        }
     }
-    defer response.Body.Close()
-    document, err := goquery.NewDocumentFromReader(response.Body)
-    if err != nil { log.Println("Error loading HTTP response body. ") }
-    getRow(document)
-
-
-
-    return
 }
 
-func getRow(documment *goquery.Document) (getRow string){
-    // This gets the UL Block
-    documment.Find("ul.listing-results").Each(func(index int, element *goquery.Selection) {
-        // Now get a Row, what about row count?
-        log.Print(element.Html())
+func getIdList(documment *goquery.Document) (getIdList []int64){
+    documment.Find("ul.listing-results").Each(func(index int, elementList *goquery.Selection) {
+        elementList.Find("li").Each(func(index int, element *goquery.Selection) {
+            itemId, valid := element.Attr("data-listing-id")
+            if valid {
+                idValue ,_ := strconv.ParseInt(itemId,0,64)
+                getIdList = append(getIdList,idValue)
+            }
+        })
     })
+    log.Println(getIdList)
 
     return
 }
 
-func parseRow(row string) {
-    log.Print(row)
-}
-
-
-func getUrls() []string {
-    return []string{"https://www.zoopla.co.uk/for-sale/houses/manchester/?q=Manchester&radius=0&results_sort=newest_listings&search_source=refine",
-           "https://www.zoopla.co.uk/for-sale/houses/manchester/?identifier=manchester&property_type=houses&q=Manchester&search_source=refine&radius=0&pn=2"}
-}
-
-
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) (ret int) {
-    defer func() {
-        r := recover()
-        if r != nil { log.Println("File error Ignored:", r) }
-    }()
-    defer waitGroup.Done()
-    // Create the file
-    out, err := os.Create(filepath)
-    if err != nil {
-        log.Print(err)
-        ret = 0
-    }
-    defer out.Close()
-
-    // Get the data
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Print(err)
-        ret = 0
-    }
-    defer resp.Body.Close()
-
-    // Write the body to file
-    _, err = io.Copy(out, resp.Body)
-    if err != nil {
-        log.Print(err)
-        ret = 0
-    } else {
-        ret = 1
-    }
+func getTotalPages(documment *goquery.Document) (getTotalPages int) {
+    getTotalPages = 0
+    documment.Find("div.paginate").Each(func(index int, elementList *goquery.Selection) {
+        elementList.Find("a").Each(func(index int, element *goquery.Selection) {
+            page := element.Text()
+            currentPage, _ := strconv.Atoi(page)
+            if currentPage > getTotalPages {
+                getTotalPages = currentPage
+            }
+        })
+    })
 
     return
 }
